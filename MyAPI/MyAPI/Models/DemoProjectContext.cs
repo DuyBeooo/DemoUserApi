1146b51e8174b17369462bfb468edc80b8784f4a
duybeoo <nqd3092@gmail.com>
2022-08-31 19:37:18 +0700

    Backend

---
+++ MyAPI/MyAPI/Models/DemoProjectContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace MyAPI.Models
+{
+    public partial class DemoProjectContext : DbContext
+    {
+       
+        public DemoProjectContext(DbContextOptions<DemoProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<UserAccount> UserAccounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<UserAccount>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK__UserAcco__1788CC4C42572A0F");
+
+                entity.ToTable("UserAccount");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
