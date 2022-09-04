516f141d6e0b4270fdd2d1f6515ed7538f3025f0
duybeoo <nqd3092@gmail.com>
2022-09-05 00:20:19 +0700

    New

---
+++ MyAPI3/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using MyAPI3.DBContext;
+using MyAPI3.Models;
+using MyAPI3.Repository;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAPI3.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _repository;
+
+        public UserController(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUser()
+        {
+            var list = _repository.GetAll();
+            return Ok(list);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(string id)
+        {
+            var user = _repository.GetUserById(id);
+            if(user == null)
+                return NotFound("This is is not exist");
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            if (_repository.AddUser(user))
+                return Ok("Add success");
+            return BadRequest("Add fail");
+        }
+
+        [HttpPut]
+        public IActionResult EditUser(User user)
+        {
+            if(_repository.EditUser(user))
+                return Ok("Update success");
+            return BadRequest("Update fail");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(string id)
+        {
+            if (_repository.DeleteUser(id))
+                return Ok("Delete success");
+            return BadRequest("Delete fail");
+        }
+
+    }
+}
---
+++ MyAPI3/Models/LoginRespondFormat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyAPI3.Models
+{
+    public class LoginRespondFormat
+    {
+        public Data Data { get; internal set; }
+        public bool Error { get; set; }
+        public string Message { get; set; }
+        public int StatudeCode { get; set; }
+    }
+
+    public class Data
+    {
+        public string AccessToken { get; set; }
+        public string TypeToken { get; set; }
+
+    }
+
+    public class Main
+    {
+        LoginRespondFormat l = new()
+        {
+            Data = new Data() { AccessToken = "aaa", TypeToken = "bbb" },
+            Error = false,
+            Message = "No mess",
+            StatudeCode = 200
+        };
+
+    }
+}
